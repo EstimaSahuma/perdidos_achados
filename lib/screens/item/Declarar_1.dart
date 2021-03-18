a2f18afdcccb493fa5d1e813d02f546389dcddd0
EstimaSahuma <estimaetilenoglicol@gmail.com>
2021-03-18 11:51:13 +0100

    jjjs

--- lib/screens/item/Declarar_1.dart
+++ lib/screens/item/Declarar_1.dart
@@ -82,16 +82,17 @@ class _DeclararForm_1State extends State<DeclararForm_1> {
                 color: Colors.green,
               ),
               hintText: 'Escolha o que Procuras/Encotraste ',
-            ),/* 
+            ),
             validator: (value) {
               return value.isEmpty ? 'é necessario o tipo.' : value;
-            }, */
+            },
             onSaved: (value) {
-              if (value.toString() == "PESSOA")
+              /* if (value.toString() == "PESSOA")
                 return _valueGroupTipo(1);
               else if (value.toString() == "ANIMAL")
                 return _valueGroupTipo(2);
-              else if (value.toString() == "OBJECTO") return _valueGroupTipo(3);
+              else if (value.toString() == "OBJECTO") return _valueGroupTipo(3); */
+              this.texto = value;
             },
           ),
         ),
@@ -109,241 +110,243 @@ class _DeclararForm_1State extends State<DeclararForm_1> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 30.0,
-            ),
-            /* ---------------------------------------------------------------- */
-            Text(
-              "Estado do Item",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Radio(
-                    value: 1,
-                    activeColor: Colors.orange,
-                    groupValue: valueGroupEstado,
-                    onChanged: (value) {
-                      setState(() {
-                        this.valueGroupEstado = value;
-                      });
-                    }),
-                Text(
-                  "Perdido",
-                  style: TextStyle(
-                      color: Colors.blueGrey[600],
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 70.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    Radio(
-                        value: 2,
-                        activeColor: Colors.orange,
-                        groupValue: valueGroupEstado,
-                        onChanged: (value) {
-                          setState(() {
-                            this.valueGroupEstado = value;
-                          });
-                        }),
-                    Text(
-                      "Achado",
-                      style: TextStyle(
-                          color: Colors.blueGrey[600],
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            /* ---------------------------------------------------------------- */
-            Divider(
-              color: Colors.blueGrey[600],
-            ),
-            /* ------------------------------------------------------------ */
-            SizedBox(
-              height: 20.0,
-            ),
-            _buildTipo(),
-            /* ---------------------------------------------------------------- */
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Tem Recompensa ?",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Radio(
-                    value: 1,
-                    activeColor: Colors.orange,
-                    groupValue: valueGroupRecompensa,
-                    onChanged: (value) {
-                      setState(() {
-                        this.valueGroupRecompensa = value;
-                      });
-                    }),
-                Text(
-                  "Sim",
-                  style: TextStyle(
-                      color: Colors.blueGrey[600],
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 70.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    Radio(
-                        value: 2,
-                        activeColor: Colors.orange,
-                        groupValue: valueGroupRecompensa,
-                        onChanged: (value) {
-                          setState(() {
-                            this.valueGroupRecompensa = value;
-                          });
-                        }),
-                    Text(
-                      "Não",
-                      style: TextStyle(
-                          color: Colors.blueGrey[600],
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            /* ---------------------------------------------------------------- */
-            Divider(
-              color: Colors.blueGrey[600],
-            ),
-            /* ---------------------------------------------------------------- */
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Usarás os teus Contactos e Localização?",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Radio(
-                    value: 1,
-                    activeColor: Colors.orange,
-                    groupValue: valueGroupContacto,
-                    onChanged: (value) {
-                      setState(() {
-                        this.valueGroupContacto = value;
-                      });
-                    }),
-                Text(
-                  "Sim",
-                  style: TextStyle(
-                      color: Colors.blueGrey[600],
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 70.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    Radio(
-                        value: 2,
-                        activeColor: Colors.orange,
-                        groupValue: valueGroupContacto,
-                        onChanged: (value) {
-                          setState(() {
-                            this.valueGroupContacto = value;
-                          });
-                        }),
-                    Text(
-                      "Não",
-                      style: TextStyle(
-                          color: Colors.blueGrey[600],
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            /* ---------------------------------------------------------------- */
-            SizedBox(
-              height: 30.0,
-            ),
-            Center(
-              child: RaisedButton(
-                elevation: 5.0,
-                onPressed: /* validateAndSave, */
-                    () {
-                  /* if (valueGroupTipo != 0) */
-                    print("valueGroupTipo");
-                    print(valueGroupEstado);
+        child: Form(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 30.0,
+              ),
+              /* ---------------------------------------------------------------- */
+              Text(
+                "Estado do Item",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Radio(
+                      value: 1,
+                      activeColor: Colors.orange,
+                      groupValue: valueGroupEstado,
+                      onChanged: (value) {
+                        setState(() {
+                          this.valueGroupEstado = value;
+                        });
+                      }),
+                  Text(
+                    "Perdido",
+                    style: TextStyle(
+                        color: Colors.blueGrey[600],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 70.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Radio(
+                          value: 2,
+                          activeColor: Colors.orange,
+                          groupValue: valueGroupEstado,
+                          onChanged: (value) {
+                            setState(() {
+                              this.valueGroupEstado = value;
+                            });
+                          }),
+                      Text(
+                        "Achado",
+                        style: TextStyle(
+                            color: Colors.blueGrey[600],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              /* ---------------------------------------------------------------- */
+              Divider(
+                color: Colors.blueGrey[600],
+              ),
+              /* ------------------------------------------------------------ */
+              SizedBox(
+                height: 20.0,
+              ),
+              _buildTipo(),
+              /* ---------------------------------------------------------------- */
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Tem Recompensa ?",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Radio(
+                      value: 1,
+                      activeColor: Colors.orange,
+                      groupValue: valueGroupRecompensa,
+                      onChanged: (value) {
+                        setState(() {
+                          this.valueGroupRecompensa = value;
+                        });
+                      }),
+                  Text(
+                    "Sim",
+                    style: TextStyle(
+                        color: Colors.blueGrey[600],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 70.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Radio(
+                          value: 2,
+                          activeColor: Colors.orange,
+                          groupValue: valueGroupRecompensa,
+                          onChanged: (value) {
+                            setState(() {
+                              this.valueGroupRecompensa = value;
+                            });
+                          }),
+                      Text(
+                        "Não",
+                        style: TextStyle(
+                            color: Colors.blueGrey[600],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              /* ---------------------------------------------------------------- */
+              Divider(
+                color: Colors.blueGrey[600],
+              ),
+              /* ---------------------------------------------------------------- */
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Usarás os teus Contactos e Localização?",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Radio(
+                      value: 1,
+                      activeColor: Colors.orange,
+                      groupValue: valueGroupContacto,
+                      onChanged: (value) {
+                        setState(() {
+                          this.valueGroupContacto = value;
+                        });
+                      }),
+                  Text(
+                    "Sim",
+                    style: TextStyle(
+                        color: Colors.blueGrey[600],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 70.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Radio(
+                          value: 2,
+                          activeColor: Colors.orange,
+                          groupValue: valueGroupContacto,
+                          onChanged: (value) {
+                            setState(() {
+                              this.valueGroupContacto = value;
+                            });
+                          }),
+                      Text(
+                        "Não",
+                        style: TextStyle(
+                            color: Colors.blueGrey[600],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              /* ---------------------------------------------------------------- */
+              SizedBox(
+                height: 30.0,
+              ),
+              Center(
+                child: RaisedButton(
+                  elevation: 5.0,
+                  onPressed: /* validateAndSave, */
+                      () {
+                    /* if (valueGroupTipo != 0) */
+                    print("valueGroupTipo 1");
+                    print(texto);
                     print("valueGroupTipo");
                     Navigator.push(context,
                         MaterialPageRoute(builder: (_) => ObjectoForm()));
-                  /* else
-                    texto = "Escolher o Tipo de Item (Pessoa/Animal/Objecto)"; */
-                },
-                padding:
-                    EdgeInsets.symmetric(horizontal: 120.0, vertical: 15.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: Colors.green[600],
-                child: Text(
-                  'Continuar...',
-                  style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 1.5,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'OpenSans',
+                    /* else
+                      texto = "Escolher o Tipo de Item (Pessoa/Animal/Objecto)"; */
+                  },
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 120.0, vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Colors.green[600],
+                  child: Text(
+                    'Continuar...',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.5,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'OpenSans',
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(texto,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ))
-          ],
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(texto,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 14.0,
+                  ))
+            ],
+          ),
         ),
       ),
     );
